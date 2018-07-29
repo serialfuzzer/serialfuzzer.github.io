9abd794fe848a7c044c73c31b10e629c560637d2
TheN008 <suyogkhanal4@gmail.com>
2018-07-29 22:17:05 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 window.onload = function () {
   var container = document.querySelector(".container");
+  var loading = document.querySelector(".overlay");
   var button = document.querySelector(".know");
   var nextPageButton = document.querySelector(".nextPage");
   var education = document.querySelector(".education");
   var skills = document.querySelector(".skills");
+  overlay.style.display = "none";
   button.addEventListener("click", function() {
     container.style.display = "none";
     education.style.display = "block";
