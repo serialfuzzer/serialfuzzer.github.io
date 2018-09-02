adc22e644231ec1760817a4b7c39418d6bf2f043
TheN008 <suyogkhanal4@gmail.com>
2018-09-02 20:32:57 +0530

    Added min. 4 seconds time for load

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-  $('.container').css('visibility', 'visible');
-  $('.loader').css('display', 'none');
-  particlesJS.load('particles-js', '/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+  setTimeout(function(){
+    $('.container').css('visibility', 'visible');
+    $('.loader').css('display', 'none');
+    particlesJS.load('particles-js', '/particles.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
+  }, 4000);
 });
