4a921ec5ac9fc590ac9d8954536ac0805446abb1
TheN008 <suyogkhanal4@gmail.com>
2018-09-02 20:22:06 +0530

    Added particles.js

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
   $('.container').css('visibility', 'visible');
   $('.loader').css('display', 'none');
+  particlesJS.load('particles-js', '/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
 });
